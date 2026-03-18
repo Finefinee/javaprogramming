c71744d1f8945b07a89ed87e53be26e9e8eb7294
김연호 <kimfine000@gmail.com>
2026-03-18 10:40:32 +0900

    3.18

---
+++ src/scanner/ScannerEx1.java
@@ -0,0 +1,22 @@
+package scanner;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ScannerEx1 {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int hap = 0;
+
+        for (int i = 0; i < 3; i++) {
+            int number = scanner.nextInt();
+            hap += number;
+        }
+
+        System.out.println((float) hap/3);
+
+    }
+}
