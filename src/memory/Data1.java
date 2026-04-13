d6cae24122bfd4aeb03b78b88dffaff3c2f9dc8f
김연호 <kimfine000@gmail.com>
2026-04-13 21:26:11 +0900

    4.10

---
+++ src/memory/Data1.java
@@ -0,0 +1,14 @@
+package memory;
+
+public class Data1 {
+
+    private int value;
+
+    public Data1(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+}
---
+++ src/memory/static1/Data1.java
@@ -0,0 +1,12 @@
+package memory.static1;
+
+public class Data1 {
+
+    private String name;
+    public int count;
+
+    public Data1(String name) {
+        this.name = name;
+        count++;
+    }
+}
